a57eb953b40ef6ade184953dc0f05f1cc8182071
zhangcanbin <zhangcanbin@hongwangweb.com>
2021-07-30 16:34:28 +0800

    添加样式

--- python-django/mysite/polls/admin.py
+++ python-django/mysite/polls/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+# admin.site.register(Question)
+
+# 定制admin模块
+
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
