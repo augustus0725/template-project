5666354c31a00fd94a6f4f0e876cdca6491c1a12
zhangcanbin <zhangcanbin@hongwangweb.com>
2021-08-11 15:35:33 +0800

    增加版本号

--- python-antlr/orax/docs/source/conf.py
+++ python-antlr/orax/docs/source/conf.py
@@ -28,7 +28,7 @@ copyright = '2021, 江苏红网股份有限公司'
 author = '江苏红网'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
