08575ba5475481d5e1d696d9c96c76e00edb28c4
zhangcanbin <zhangcanbin@hongwangweb.com>
2021-08-13 10:15:18 +0800

    添加django的例子

---
+++ python-django-apps/cms/readme.txt
@@ -0,0 +1,3 @@
+python3 -m venv venv
+pip3 install djangocms-installer
+djangocms mysite
--- simple-django/hello.py
+++ simple-django/hello.py
@@ -3,6 +3,7 @@ import sys
 
 from django.conf import settings
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 settings.configure(
@@ -22,8 +23,11 @@ def index(request):
 
 
 urlpatterns = (
+    url(r'^image/(?P<width>[0-9]+)X()$', index),
     url(r'^$', index),
 )
+
+application = get_wsgi_application()
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
