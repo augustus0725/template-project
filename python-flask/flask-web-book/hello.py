bb7c5ae4779ffb41269a3d858b4ef8d78244da8b
zhangcanbin <zhangcanbin@hongwangweb.com>
2021-08-15 23:03:07 +0800

    学习使用flask-wtf封装的表单功能

--- python-flask/flask-web-book/hello.py
+++ python-flask/flask-web-book/hello.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 # bootstrap
 bootstrap = Bootstrap(app)
@@ -13,9 +18,18 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        # name = form.name.data
+        # form.name.data = ''
+        old_name = session.get('name')
+        if old_name and old_name != form.name.data:
+            flash("看来你已经修改了名称.")
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -32,3 +46,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- python-flask/flask-web-book/readme.txt
+++ python-flask/flask-web-book/readme.txt
@@ -13,3 +13,5 @@ flask run
 pip3 install flask-bootstrap
 -- 不同国家的时区不一样, 一种通用的方案是： 后台服务的时间采用UTC, utc时间发送给Web浏览器的时候做本地化的转换
 pip3 install flask-moment
+-- 使用表单
+pip3 install flask-wtf
