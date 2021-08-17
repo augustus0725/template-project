6d3c04446d52b5090f072b92e691a93ad0107c3f
zhangcanbin <zhangcanbin@hongwangweb.com>
2021-08-17 09:41:23 +0800

    添加修改profile

--- python-flask/flask-app/app/main/views.py
+++ python-flask/flask-app/app/main/views.py
@@ -2,10 +2,11 @@
 from datetime import datetime
 
 from flask import session, flash, redirect, url_for, render_template
+from flask_login import login_required, current_user
 
 from app import db
 from app.main import main
-from app.main.forms import NameForm
+from app.main.forms import NameForm, EditProfileForm
 from app.models import User
 
 
@@ -18,3 +19,21 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
