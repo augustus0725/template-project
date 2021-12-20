bc5631ab92407790f47ee344866f47dd796b9c54
zhangcanbin <zhangcanbin@hongwangweb.com>
2021-12-20 18:26:01 +0800

    添加hive的权限控制

--- cdh-kerberos-template/src/main/java/com/sabo/cdh/kerberos/hive/HiveTemplate.java
+++ cdh-kerberos-template/src/main/java/com/sabo/cdh/kerberos/hive/HiveTemplate.java
@@ -24,17 +24,22 @@ public class HiveTemplate {
         // kerberos auth as kinit
         Configuration config = new Configuration();
         System.setProperty("java.security.krb5.conf", "hive-conf/krb5.conf");
-        System.setProperty("sun.security.krb5.debug", "true");
+//        System.setProperty("sun.security.krb5.debug", "true");
         config.addResource(new Path("hive-conf", "core-site.xml"));
         UserGroupInformation.setConfiguration(config);
-        UserGroupInformation.loginUserFromKeytab("vagrant@hongwang.com", "hive-conf/vagrant.keytab");
+        UserGroupInformation.loginUserFromKeytab("hive/bd04.hw.com@hongwang.com", "hive-conf/hive.keytab");
 
         // JDBC
         Class.forName(JDBC_DRIVER_NAME);
         Connection con = DriverManager.getConnection(CONNECTION_URL);
 
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM default.t1");
+//        stmt.execute("CREATE ROLE role_create_db");
+//        stmt.execute("GRANT SELECT ON SERVER server1 TO ROLE role_create_db"); // -> 能看到库表 默认能查所有的表 字段
+//        stmt.execute("GRANT ROLE role_create_db TO GROUP hive");
+//        stmt.execute("GRANT CREATE ON SERVER server1 TO ROLE role_create_db"); // create database, 拥有库的有所有的权限
+        stmt.execute("REVOKE CREATE ON DATABASE c01 FROM ROLE role_create_db");
+        ResultSet rs = stmt.executeQuery("SHOW ROLES");
 
         System.out.println("\n== Begin Query Results ======================");
 
