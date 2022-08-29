83afe90a4da359bc0f7f77326a5687bfaddb3518
zhangcanbin <zhangcanbin@hongwangweb.com>
2022-08-29 10:59:28 +0800

    rootSaga可以让一堆子的saga处于接收状态，也是问题分解的方法

--- web/JavaScript/redux/parsnip/src/sagas.js
+++ web/JavaScript/redux/parsnip/src/sagas.js
@@ -1,3 +1,16 @@
+import { fork } from 'redux-saga/effects';
+
 export default function* rootSaga() {
     console.log('This is root sage.');
+    // fork 能让程序不阻塞
+    yield fork(watchFetchTasks);
+    yield fork(watchFetchTasks1);
+}
+
+function* watchFetchTasks() {
+    console.log('Watch fetch tasks......');
+}
+
+function* watchFetchTasks1() {
+    console.log('1 Watch fetch tasks......');
 }
