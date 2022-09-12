8b2629d61d1ab89f6134c4c679c316f703fece59
Canbin Zhang <yesido0725@gmail.com>
2022-09-12 11:17:42 +0800

    使用useReducer, redux way hook

--- web/JavaScript/react-hooks/hooks-app/src/App.js
+++ web/JavaScript/react-hooks/hooks-app/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useReducer } from 'react';
 import { ThemeContext } from './context';
+
+const initialState = {count: 0};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
+  }
+}
+
 
 function App() {
   const [count, setCount] = useState(0);
   const theme = useContext(ThemeContext);
+  // reducer (redux way)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     // 修改 DOM
@@ -18,8 +34,10 @@ function App() {
       <div> Learn react hooks </div>
       <div> --------------------------- </div>
       <div style={{ background: theme.background, color: theme.foreground }}> {count} </div>
+      <div style={{ background: theme.background, color: theme.foreground }}> redux way count: {state.count} </div>
       <div> --------------------------- </div>
       <button onClick={() => setCount(count + 1)}> Click me! </button>
+      <button onClick={() => dispatch({type: 'increment'})}> Click me with reducer! </button>
     </div>
   );
 }
