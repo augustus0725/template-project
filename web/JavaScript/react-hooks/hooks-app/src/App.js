0ccb4dd462d48ceccecbf36f2ab0eef9e1e936e8
Canbin Zhang <yesido0725@gmail.com>
2022-09-12 10:38:52 +0800

    添加useEffect hook的例子

--- web/JavaScript/react-hooks/hooks-app/src/App.js
+++ web/JavaScript/react-hooks/hooks-app/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // 修改 DOM
+    // 字符串里的变量采用ES6语法， 字符串模板
+    // 完成对 DOM 的更改后运行你的“副作用”函数
+    document.title = `You have clicked ${count} times`;
+  });
 
   return (
     <div className="App">
