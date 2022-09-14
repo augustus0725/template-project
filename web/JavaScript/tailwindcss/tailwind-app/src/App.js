ebbbe523f53b5bd17f3ad63e5ce86286d5395fed
zhangcanbin <zhangcanbin@hongwangweb.com>
2022-09-14 16:04:59 +0800

    添加w-2 h-2 p-2 block flex的示例

--- web/JavaScript/tailwindcss/tailwind-app/src/App.js
+++ web/JavaScript/tailwindcss/tailwind-app/src/App.js
@@ -61,6 +61,35 @@ function App() {
         <p>Look. If you think this is...</p>
       </div>
       <div>--------------------------------------------------------------------------------------------------------------------------------------------------------</div>
+      {/* 换行之后, 颜色渐变怎么处理, slice-> 分段完成渐变 or clone 每段都产生渐变效果 */}
+      <div>换行之后, 颜色渐变怎么处理, slice- 分段完成渐变 or clone 每段都产生渐变效果</div>
+      <div>
+        <span className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2">
+          Hello<br />
+          World
+        </span> <br />
+        <span className="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2">
+          Hello<br />
+          World
+        </span>
+      </div>
+      {/* 定义box的宽 w-32 和高 h-32, padding - p-4, border-2 */}
+      <div>定义box的宽 w-32 和高 h-32, padding - p-4, border-2 </div>
+      <div className="w-32 h-32 border-2 border-black"></div>
+      {/* 对比一下，加上box-border */}
+      {/* 对比一下，box-border 限制了元素总的大小 */}
+      {/*          box-content 限制了内容的大小， 总大小要加上margin， border, padding */}
+      <div class="box-border h-32 w-32 p-4 border-2 border-red-500"></div>
+      <div className="box-content h-32 w-32 border-2 border-yellow-300"></div>
+      <div>-bolock 级别的flex容器-----------------------------------------------------------------------------------------------------------------------------------------</div>
+      <div className="flex items-center w-64 h-32">
+        <img src={logo} alt="show the logo"></img>
+        <div>
+          <strong>Andrew Alfred</strong>
+          <span>Technical advisor</span>
+        </div>
+      </div>
+
 
     </>
   );
