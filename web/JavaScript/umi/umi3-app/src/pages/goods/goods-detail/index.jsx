40bdfd704a79899f15da9340eb22daa3025593d4
zhangcanbin <zhangcanbin@hongwangweb.com>
2022-09-17 15:22:30 +0800

    添加跳转和跳转传参的例子

--- web/JavaScript/umi/umi3-app/src/pages/goods/goods-detail/index.jsx
+++ web/JavaScript/umi/umi3-app/src/pages/goods/goods-detail/index.jsx
@@ -1,5 +1,20 @@
-export default function() {
+import { useParams, useLocation, useRouteMatch } from "umi";
+
+export default function ({ location: {search}, match: { params: {id} } }) {
+  const params = useParams();
+  // const {id} = useParams();
+  const location = useLocation();
+  // const {search} = useLocation();
+
   return (
-    <div> It's detail for the goods. </div>
+    <>
+      <div> It's detail for the goods. </div>
+      <div> 用路由上下文获取path的参数: {id} </div>
+      <div> 用路由上下文获取query的参数: {search} </div>
+      <br/>
+      <br/>
+      <div> 用hook获取path的参数: {params.id} </div>
+      <div> 用hook获取query的参数: {location.search} </div>
+    </>
   );
 }
--- web/JavaScript/umi/umi3-app/src/pages/index.jsx
+++ web/JavaScript/umi/umi3-app/src/pages/index.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import styles from './index.less';
 import { Button } from 'antd';
 import { Button as V2Button } from 'antd-mobile';
+import { useHistory } from 'umi';
 
-export default function IndexPage() {
+export default function IndexPage({history}) {
   const [count, setCount] = useState(0);
+  const hookHistory = useHistory();
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
@@ -13,6 +16,13 @@ export default function IndexPage() {
       <div className={styles.a1}>count is : {count}</div>
       <Button type="primary" onClick={() => setCount(count+1)}>Add</Button>
       <V2Button type="primary" size="small" inline>V2-Mobile</V2Button>
+      {/* 使用编程做跳转 */}
+      <br/>
+      <br/>
+      <Button type="primary" onClick={() => history.push('/login')}>login with history</Button>
+      <br/>
+      <br/>
+      <Button type="primary" onClick={() => hookHistory.push('/login', {a:3})}>login with history hooks</Button>
     </div>
   );
 }
