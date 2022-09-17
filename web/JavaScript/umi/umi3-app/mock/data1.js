00ca3cca511249aa8b6d3d194d9f2f4e87cb5352
zhangcanbin <zhangcanbin@hongwangweb.com>
2022-09-17 15:36:56 +0800

    用函数的方式做mock

--- web/JavaScript/umi/umi3-app/mock/data1.js
+++ web/JavaScript/umi/umi3-app/mock/data1.js
@@ -2,5 +2,9 @@ export default {
   'GET /umi/goods': [
     {id: 1, name: '西瓜'},
     {id: 2, name: '西红柿'},
-  ]
+  ],
+  // 函数的方式
+  'POST /api/users/create': (req, res) => {
+    res.send("ok");
+  },
 }
