b90358c485e30435db69177154d49508dd801bbe
zhangcanbin <zhangcanbin@hongwangweb.com>
2022-09-17 12:23:15 +0800

    添加没有提交的文件

---
+++ web/JavaScript/umi/umi3-app/src/pages/goods/index.jsx
@@ -0,0 +1,7 @@
+const Goods = () => {
+  return <div>
+    <h1> Hello, it's Goods page. </h1>
+  </div>;
+};
+
+export default Goods;
---
+++ web/JavaScript/umi/umi3-app/src/pages/goods/list/index.js
@@ -0,0 +1,5 @@
+export default function() {
+  return (
+    <div> It's good list. </div>
+  );
+}
