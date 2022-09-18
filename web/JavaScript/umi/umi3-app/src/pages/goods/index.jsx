6d0f4489f32e1d8faed9370d28050ba016a72ec4
Canbin Zhang <yesido0725@gmail.com>
2022-09-18 08:25:19 +0800

    使用umi hook不用connect来关联state

--- web/JavaScript/umi/umi3-app/src/pages/goods/index.jsx
+++ web/JavaScript/umi/umi3-app/src/pages/goods/index.jsx
@@ -1,6 +1,27 @@
+import { useDispatch, useSelector } from 'umi';
+import { Button } from 'antd';
+
 const Goods = () => {
+  const dispatch = useDispatch();
+  const { title, login } = useSelector((state) => ({
+    title: state.global.title,
+    login: state.global.login,
+  }));
+
+  // 尽量用hook， 不用connect来关联状态
+
   return <div>
-    <h1> Hello, it's Goods page. </h1>
+    <h1> Hello, it's Goods page. Title is : {title} </h1>
+    <div> Login is : {login.toString()} </div>
+    <Button type="primary" onClick={() => {
+      dispatch({
+        type: 'global/login',
+        payload: {
+          username: 'admin',
+          password: '123',
+        }
+      });
+    }}>Login async</Button>
   </div>;
 };
 
