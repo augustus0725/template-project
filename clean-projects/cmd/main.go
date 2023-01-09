556083c12f41b471dc5e334f83ed79742a04cd7d
zhangcanbin@hongwangweb.com <zhangcanbin@hongwangweb.com>
2023-01-09 10:43:03 +0800

    改造了一下，使用goroutines

--- clean-projects/cmd/main.go
+++ clean-projects/cmd/main.go
@@ -6,12 +6,14 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 var (
 	cmd  = kingpin.New("clean-projects", "Clean projects like java, node")
 	path = cmd.Flag("path", "root path to clean.").
 		Default(".").String()
+	wg sync.WaitGroup
 )
 
 func main() {
@@ -23,11 +25,11 @@ func main() {
 		fmt.Println(err)
 	}
 	scanAndClean(*path)
-
 }
 
 func scanAndClean(path string) {
 	var err error
+	var toDelete []string
 	err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
 		// java project
 		if d.IsDir() && "target" == filepath.Base(path) {
@@ -35,29 +37,55 @@ func scanAndClean(path string) {
 			_, err = os.Stat(filepath.Join(filepath.Dir(path), "pom.xml"))
 			if nil == err {
 				// exists
-				fmt.Println("deleting ", path)
-				err := os.RemoveAll(path)
-				if err != nil {
-					return err
-				}
+				fmt.Println("Find path to be deleted :", path)
+				toDelete = append(toDelete, path)
+			} else {
+				fmt.Println("Java project not valid: ", path)
+				err = nil
 			}
+			return filepath.SkipDir
 		}
 		// nodejs project
 		if d.IsDir() && "node_modules" == filepath.Base(path) {
 			// package.json exists
 			_, err = os.Stat(filepath.Join(filepath.Dir(path), "package.json"))
 			if nil == err {
-				// exists
-				fmt.Println("deleting ", path)
-				err := os.RemoveAll(path)
-				if err != nil {
-					return err
-				}
+				fmt.Println("Find path to be deleted :", path)
+				toDelete = append(toDelete, path)
+			} else {
+				fmt.Println("nodejs project not valid: ", path)
+				err = nil
 			}
+			return filepath.SkipDir
+		}
+		// python project
+		if d.IsDir() && "__pycache__" == filepath.Base(path) {
+			toDelete = append(toDelete, path)
+			fmt.Println("Find path to be deleted :", path)
+			return filepath.SkipDir
+		}
+		// delete .idea .vscode
+		if d.IsDir() && (".idea" == filepath.Base(path) || ".vscode" == filepath.Base(path)) {
+			toDelete = append(toDelete, path)
+			fmt.Println("Find path to be deleted :", path)
+			return filepath.SkipDir
 		}
 		return err
 	})
 	if err != nil {
 		return
 	}
+	wg.Add(len(toDelete))
+	for _, v := range toDelete {
+		v := v
+		go func() {
+			defer wg.Done()
+			fmt.Println("deleting ", v)
+			err := os.RemoveAll(v)
+			if err != nil {
+				return
+			}
+		}()
+	}
+	wg.Wait()
 }
