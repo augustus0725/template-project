88cab0b559cd5f52cd0c8eb65b21209c7b008f6f
zhangcanbin@hongwangweb.com <zhangcanbin@hongwangweb.com>
2023-02-03 14:33:42 +0800

    create static lib and use it

---
+++ c&cpp/clion/c_link_lib/include/hello.h
@@ -0,0 +1,8 @@
+//
+// Created by darwin on 2023/2/3.
+//
+
+#ifndef C_LINK_LIB_HELLO_H
+#define C_LINK_LIB_HELLO_H
+void print();
+#endif //C_LINK_LIB_HELLO_H
---
+++ c&cpp/clion/c_link_lib/src/hello.c
@@ -0,0 +1,10 @@
+//
+// Created by darwin on 2023/2/3.
+//
+
+#include <stdio.h>
+#include "hello.h"
+
+void print() {
+    printf("Hello world!\n");
+}
