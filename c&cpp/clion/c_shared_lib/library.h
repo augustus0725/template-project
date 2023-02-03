53f7765e42f6783798635d825dd29cd3edd8dc97
zhangcanbin@hongwangweb.com <zhangcanbin@hongwangweb.com>
2023-02-03 11:43:04 +0800

    c cpp shared lib

---
+++ c&cpp/clion/c_shared_lib/library.h
@@ -0,0 +1,6 @@
+#ifndef C_SHARED_LIB_LIBRARY_H
+#define C_SHARED_LIB_LIBRARY_H
+
+void hello(void);
+
+#endif //C_SHARED_LIB_LIBRARY_H
---
+++ c&cpp/clion/cpp_shared_lib/library.h
@@ -0,0 +1,6 @@
+#ifndef CPP_SHARED_LIB_LIBRARY_H
+#define CPP_SHARED_LIB_LIBRARY_H
+
+void hello();
+
+#endif //CPP_SHARED_LIB_LIBRARY_H
