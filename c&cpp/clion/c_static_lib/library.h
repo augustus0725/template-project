0b228cd96987e110429aa62faa69210439d7f684
zhangcanbin@hongwangweb.com <zhangcanbin@hongwangweb.com>
2023-02-03 11:39:18 +0800

    c cpp lib

---
+++ c&cpp/clion/c_static_lib/library.h
@@ -0,0 +1,6 @@
+#ifndef C_STATIC_LIB_LIBRARY_H
+#define C_STATIC_LIB_LIBRARY_H
+
+void hello(void);
+
+#endif //C_STATIC_LIB_LIBRARY_H
---
+++ c&cpp/clion/cpp_static_lib/library.h
@@ -0,0 +1,6 @@
+#ifndef CPP_STATIC_LIB_LIBRARY_H
+#define CPP_STATIC_LIB_LIBRARY_H
+
+void hello();
+
+#endif //CPP_STATIC_LIB_LIBRARY_H
