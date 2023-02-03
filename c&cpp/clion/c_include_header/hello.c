f592093879433534df6d9154c56a0348a6743d9c
zhangcanbin@hongwangweb.com <zhangcanbin@hongwangweb.com>
2023-02-03 13:55:37 +0800

    add c include demo

---
+++ c&cpp/clion/c_include_header/hello.c
@@ -0,0 +1,9 @@
+//
+// Created by darwin on 2023/2/3.
+//
+#include <stdio.h>
+#include "include/hello.h"
+
+void hello() {
+    printf("Hello world!\n");
+}
---
+++ c&cpp/clion/c_include_header/include/hello.h
@@ -0,0 +1,8 @@
+//
+// Created by darwin on 2023/2/3.
+//
+
+#ifndef C_INCLUDE_HEADER_HELLO_H
+#define C_INCLUDE_HEADER_HELLO_H
+void hello();
+#endif //C_INCLUDE_HEADER_HELLO_H
