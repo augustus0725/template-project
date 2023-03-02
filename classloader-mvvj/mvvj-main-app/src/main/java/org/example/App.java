bd598687c43b8273db0b4fa9c6377a8808a5d3cb
canbin.zhang <yesido0725@gmail.com>
2023-03-02 15:52:53 +0800

    修改classloader的例子

--- classloader-mvvj/mvvj-main-app/src/main/java/org/example/App.java
+++ classloader-mvvj/mvvj-main-app/src/main/java/org/example/App.java
@@ -8,10 +8,16 @@ import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        new ExecutorProxy("mvvj-app01\\target", "org.example.MvvjApp").execute("sabo");
-        new ExecutorProxy("mvvj-app02\\target", "org.example.MvvjApp").execute("sabo");
-        Connection connection = new JdbcProxy(new StandardExecutorClassLoader("D:\\one")).getConnection("org.postgresql.Driver",
-                "jdbc:postgresql://xxx.xxx.0.xxx:5432/xxxx", "xxxxx", "xxxx");
-        assert connection != null;
+        new ExecutorProxy("mvvj-app01/target", "org.example.MvvjApp").execute("sabo");
+        new ExecutorProxy("mvvj-app02/target", "org.example.MvvjApp").execute("sabo");
+        JdbcProxy jdbcProxy = new JdbcProxy(new StandardExecutorClassLoader("./drivers"));
+
+        for (int i = 0; i < 10; i++) {
+            Connection connection = jdbcProxy.getConnection("org.postgresql.Driver",
+                    "jdbc:postgresql://192.168.0.xxx:5432/dxp", "xxxxx", "******");
+
+            System.out.println(connection.getMetaData().getDriverName());
+            connection.close();
+        }
     }
 }
--- classloader-mvvj/mvvj-main-app/src/main/java/org/example/proxy/JdbcProxy.java
+++ classloader-mvvj/mvvj-main-app/src/main/java/org/example/proxy/JdbcProxy.java
@@ -3,19 +3,27 @@ package org.example.proxy;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JdbcProxy {
     private final StandardExecutorClassLoader classLoader;
+    private final Map<String, Driver> nameDriver = new HashMap<>();
 
     public JdbcProxy(StandardExecutorClassLoader classLoader) {
         this.classLoader = classLoader;
     }
 
+
     public Connection getConnection(String driver, String url, String user, String password) throws Exception {
-        Driver d = (Driver) Class.forName(driver, true, this.classLoader).newInstance();
-        // Driver d 是由this.classLoader 加载的, 但是DriverManager只认系统classLoader加载的驱动
-        // 所以用ShimDriver包了一层, ShimDriver是由系统classLoader加载的
-        DriverManager.registerDriver(new ShimDriver(d));
+        Driver d = nameDriver.get(driver);
+        if (null == d) {
+            d = (Driver) Class.forName(driver, true, this.classLoader).newInstance();
+            // Driver d 是由this.classLoader 加载的, 但是DriverManager只认系统classLoader加载的驱动
+            // 所以用ShimDriver包了一层, ShimDriver是由系统classLoader加载的
+            DriverManager.registerDriver(new ShimDriver(d));
+            nameDriver.put(driver, d);
+        }
         return DriverManager.getConnection(url, user, password);
     }
 }
