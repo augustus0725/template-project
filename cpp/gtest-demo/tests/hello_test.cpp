643d1899f37393ba2726e3d00372a851e7348865
canbin.zhang <yesido0725@gmail.com>
2023-05-26 19:07:17 +0800

    添加gtest用例

--- cpp/gtest-demo/tests/hello_test.cpp
+++ cpp/gtest-demo/tests/hello_test.cpp
@@ -1,10 +1,16 @@
 #include <gtest/gtest.h>
+#include "hello.h"
 
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
 // Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
+    EXPECT_STRNE("hello", "world");
 // Expect equality.
-EXPECT_EQ(7 * 6, 42);
+    EXPECT_EQ(7 * 6, 42);
+    EXPECT_EQ(3, add(1, 3));
+}
+
+TEST(HelloTest, FeatureOfAdd) {
+    EXPECT_EQ(100, add(50, 50));
 }
