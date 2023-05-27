d3f8794b651e008060efc4bb8cc4f59f1951ddbb
canbin.zhang <yesido0725@gmail.com>
2023-05-27 16:13:37 +0800

    添加cpp 日志库的例子

---
+++ cpp/log-demo/main.cpp
@@ -0,0 +1,18 @@
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+
+int main() {
+    // console
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
+    // change log pattern
+    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+    spdlog::info("Welcome to spdlog!");
+    // Rotating files
+    auto max_size = 1048576 * 5;
+    auto max_files = 3;
+    auto logger = spdlog::rotating_logger_mt("some_logger_name", "logs/rotating.txt",
+                                             max_size, max_files);
+    logger->info("Welcome to spdlog!");
+    return 0;
+}
