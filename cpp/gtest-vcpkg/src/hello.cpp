c46b88db3916b40df59ec86eb14f915ff0847d51
canbin.zhang <yesido0725@gmail.com>
2023-05-30 16:02:22 +0800

    添加vcpkg的例子

---
+++ cpp/gtest-vcpkg/src/hello.cpp
@@ -0,0 +1,8 @@
+//
+// Created by darwin on 2023/5/30.
+//
+#include "include/hello.h"
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ cpp/gtest-vcpkg/src/include/hello.h
@@ -0,0 +1,10 @@
+//
+// Created by darwin on 2023/5/30.
+//
+
+#ifndef GTEST_DEMO_HELLO_H
+#define GTEST_DEMO_HELLO_H
+
+int add(int a, int b);
+
+#endif //GTEST_VCPKG_HELLO_H
