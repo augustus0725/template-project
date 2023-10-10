0d8c9bb838b2ac7f0bfc0bbd16de6f7751625ba1
yesido0725@gmail.com <yesido0725@gmail.com>
2023-10-10 19:32:32 +0800

    指定位置添加自己的Filter

--- spring-security/lesson01/src/main/java/org/example/config/MyWebEntrySecurityConfiguration.java
+++ spring-security/lesson01/src/main/java/org/example/config/MyWebEntrySecurityConfiguration.java
@@ -7,10 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,11 +22,11 @@ import java.io.IOException;
 @Configuration
 public class MyWebEntrySecurityConfiguration extends WebSecurityConfigurerAdapter {
     /**
-     *  配置这个来改变对  entry(比如 /hello ) 的安全控制规则
+     * 配置这个来改变对  entry(比如 /hello ) 的安全控制规则
      */
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//    @Override
+    protected void configure2(HttpSecurity http) throws Exception {
 //        super.configure(http);
         http.httpBasic(c -> {
             c.authenticationEntryPoint(new AuthenticationEntryPoint() {
@@ -38,12 +42,38 @@ public class MyWebEntrySecurityConfiguration extends WebSecurityConfigurerAdapte
 //        http.authorizeRequests().anyRequest().permitAll(); /* 允许所有的请求 */
     }
 
-    @Autowired
-    private MyAuthenticationProvider provider;
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        super.configure(auth);
-        auth.authenticationProvider(provider);
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        // 添加自定义Filter, 指定位置
+        http.addFilterBefore((servletRequest, servletResponse, filterChain) -> {
+            //
+            System.out.println("Before BasicAuthenticationFilter");
+            filterChain.doFilter(servletRequest, servletResponse);
+        }, BasicAuthenticationFilter.class);
+
+        http.formLogin()
+                /*
+                .successHandler((request, response, authentication) -> {
+                    System.out.println("登录成功之后跳转");
+                    System.out.println("ContextPath: " + request.getContextPath());
+                    System.out.println("ServletPath: " + request.getServletPath());
+                    System.out.println("PathTranslated: " + request.getPathTranslated());
+                    System.out.println("PathInfo: " + request.getPathInfo());
+                }).failureHandler((request, response, exception) -> {
+                    System.out.println("登录失败之后跳转");
+                })
+                 */
+        ;
+        http.authorizeRequests().anyRequest().authenticated(); /* 所有的请求都要认证 */
     }
+
+//    @Autowired
+//    private MyAuthenticationProvider provider;
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+////        auth.authenticationProvider(provider);
+//    }
 }
---
+++ spring-security/lesson01/src/main/java/org/example/custom/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,23 @@
+package org.example.custom;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * 全局认证失败之后, 可以在这里统一处理, 添加一些提示信息
+ */
+
+@Component
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setHeader("failed", LocalDateTime.now().toString());
+    }
+}
