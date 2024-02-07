b6d6b9805b946a7b5caf690c866e1319c1121cf1
yesido0725@gmail.com <yesido0725@gmail.com>
2024-02-08 00:38:53 +0800

    简单写下用户密码登录的代码, 接口自己实现

--- web/JavaScript/next-app/auth-v5/auth.js
+++ web/JavaScript/next-app/auth-v5/auth.js
@@ -1,8 +1,38 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const config = {
-    providers: [GitHub,],
+    providers: [
+        // 第三方
+        GitHub,
+        // 表单方式
+        CredentialsProvider({
+            name: "用户密码登录",
+            credentials: {
+                username: { label: "Username" },
+                password: {  label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                console.log("credentials: ", credentials)
+                const authResponse = await fetch("/users/login", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(credentials),
+                })
+
+                if (!authResponse.ok) {
+                    return null
+                }
+
+                const user = await authResponse.json()
+
+                return user
+            },
+        }),
+    ],
     callbacks: {
       authorized({request, auth}) {
           const { pathname } = request.nextUrl;
